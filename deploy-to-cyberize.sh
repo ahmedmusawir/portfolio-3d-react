7b0b055a1cc26093f46b0bc87267a09778f03192
Ahmed Musawir <ahmed.musawir@gmail.com>
2023-04-19 12:18:49 +0800

    19Apr2023 - added deployment script on windows - using gitbash

---
+++ deploy-to-cyberize.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+LOCAL_PROJECT="portfolio-3d-react"
+PLESK_PROJECT="themoose.cyberizestaging.com"
+
+SOURCE="/c/REACT_APPS_ON_WINDOWS/$LOCAL_PROJECT/dist"
+TARGET="root@cyberizestaging.com:/var/www/vhosts/cyberizestaging.com/$PLESK_PROJECT"
+REMOTE_FOLDER="/var/www/vhosts/cyberizestaging.com/$PLESK_PROJECT/*"
+
+echo "--------------------------------------"
+echo "Building the App..."
+echo "--------------------------------------"
+npm run build
+echo "--------------------------------------"
+echo "Removing old files..."
+echo "--------------------------------------"
+ssh root@cyberizestaging.com "rm -rf $REMOTE_FOLDER"
+
+
+echo "--------------------------------------"
+echo "Transfering new files..."
+echo "--------------------------------------"
+#echo "scp -r $SOURCE/* $TARGET/"
+scp -r $SOURCE/* $TARGET/
+
+echo "--------------------------------------"
+echo "File transfer complete!"
+echo "--------------------------------------"
