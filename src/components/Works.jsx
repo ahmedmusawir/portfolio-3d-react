0b9960fd9e318ad8e36b396ebd1b6c35ee561735
Ahmed Musawir <ahmed.musawir@gmail.com>
2023-04-20 09:19:00 +0800

    20Apr2023 - final fix - works links

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,6 +7,7 @@ import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { BsFillEyeFill } from "react-icons/bs";
 
 const ProjectCard = ({
   index,
@@ -15,6 +16,7 @@ const ProjectCard = ({
   tags,
   image,
   source_code_link,
+  project_link,
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
@@ -36,13 +38,21 @@ const ProjectCard = ({
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer z-50"
             >
               <img
                 src={github}
                 alt="source code"
                 className="w-1/2 h-1/2 object-contain"
               />
+            </div>
+          </div>
+          <div className="absolute inset-0 flex justify-start m-3 card-img_hover">
+            <div
+              onClick={() => window.open(project_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <BsFillEyeFill />
             </div>
           </div>
         </div>
